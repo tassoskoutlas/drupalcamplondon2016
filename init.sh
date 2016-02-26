87cb4e7372c759c97cc74dd6d935c36cbb493e2c
Tassos Koutlas <tassos.koutlas@cameronandwilding.com>
2016-02-26 13:05:42 +0000

    Add init.sh to automatically install the drupal instance.

---
+++ init.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+
+DOCUMENTROOT=webroot
+
+if [ -z "${1}" ]; then
+
+	DRUSH="drush"
+
+else
+	
+	DRUSH=${1}
+
+fi
+
+cd ${DOCUMENTROOT}
+
+echo "Droping database ..."
+${DRUSH} sql-drop -y &> /dev/null
+
+if [ -d "sites/default/files" ]; then
+
+	echo "Clean up files directory ..."
+	rm -rf sites/default/files/*
+	rm -rf sites/defaults/files/.*
+
+fi
+
+echo "Importing database ..."
+$(${DRUSH} sql-connect) < ../db/canonical-db.sql 2> /dev/null
+
+echo "Fixing permissions"
+chmod -R 777 config
+chmod -R 777 sites/default/files
+
+echo "Done."
+exit 0
