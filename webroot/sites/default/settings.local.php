f2c2d07513d2692d2dadde23c03c36b6700f0bbb
Tassos Koutlas <tassos.koutlas@cameronandwilding.com>
2016-02-26 11:15:12 +0000

    Added drupal installation.

---
+++ webroot/sites/default/settings.local.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupalcamp',
+  'username' => 'root',
+  'password' => '1f8780bb97709b106fe9a001ff4761ab',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
